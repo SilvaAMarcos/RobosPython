9ba781a6cf26e9b3e9b1ed661bb724dcd76d15c0
SilvaAMarcos <marcos33112@gmail.com>
2022-06-07 11:04:30 -0300

    teste

---
+++ Projeto Octavios/uparvideoyoutube.py
@@ -0,0 +1,6 @@
+url = 'https://www.youtube.com/watch?v=KxGIO8vZ_l0'
+url_tratamento = url.replace('https://www.youtube.com/watch?v=', '  ')
+url_tratada = url_tratamento.strip()
+print(url_tratada)
+
+import pyautogui
